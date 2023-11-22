ee27319b811f7461de40921ead6f4a7ef105258d
Jenode Nakamoto <140191535+JuddyHopps@users.noreply.github.com>
2023-11-22 12:42:46 +0900

    Update UserUseCase.swift

--- Matching-App-Swift/UseCase/UserUseCase.swift
+++ Matching-App-Swift/UseCase/UserUseCase.swift
@@ -2,8 +2,8 @@
 //  UserUseCase.swift
 //  Matching-App-Swift
 //
-//  Created by 新納真次郎 on 2019/10/11.
-//  Copyright © 2019 新納真次郎. All rights reserved.
+//  Created by NakamanoYume on 2019/10/11.
+//  Copyright © 2019 NakamanoYume. All rights reserved.
 //
 
 import Foundation
